880683631b7d50e6997bd7fe21a21a09a6681ff1
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-12-12 18:30:55 -0500

    Add GPL license info

---
+++ README.md
@@ -0,0 +1,9 @@
+# Monitoring Dashboard
+
+This project is a custom monitoring dashboard that will serve as my main learning source for principles of Site Reliability.
+
+## License
+
+This project uses the Qt framework, which is licensed under the GNU General Public License v3 (GPL-3.0). As required by the GPL, the source code for this project is available and distributed under the same license.
+
+For more details, see the full text of the [GNU General Public License v3](https://www.gnu.org/licenses/gpl-3.0.html).
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+ /** 
+ *  Monitoring Dashboard - A simple custom monitoring dashboard.
+ *  Copyright (C) 2024 Ben Landrette
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it may be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <QApplication>
 #include <QPushButton>
 #include <QWidget>
@@ -10,6 +28,5 @@ using std::string;
 int main()
 {
 	cout << "Hello Monitoring Dashboard!" << endl;
-
 	return 0;
 }
